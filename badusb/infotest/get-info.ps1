6128448e5203c071c92c90ca5f2a3f70f781ffb4
contrxl <contrxl@protonmail.com>
2024-06-03 23:32:11 +0100

    Fixed date format error

--- badusb/infotest/get-info.ps1
+++ badusb/infotest/get-info.ps1
@@ -1,4 +1,4 @@
-$FileName = "$env:tmp/$env:USERNAME-$env:COMPUTERNAME-$(Get-Date -f dd_hh-mm-MM-yyyy).txt"
+$FileName = "$env:tmp/$env:USERNAME-$env:COMPUTERNAME-$(Get-Date -f hh-mm_dd-MM-yyyy).txt"
 
 
 function Get-userEmail{
